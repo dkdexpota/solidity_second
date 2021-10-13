92f5dc9d110d5efee07c0132ff32a15fc371fdcc
dkdexpota <artur202080202080@gmail.com>
2021-10-13 17:50:26 +0300

    first

---
+++ multiply.sol
@@ -0,0 +1,25 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract Multiply {
+
+	uint public result = 1;
+
+	constructor() public {
+		// check that contract's public key is set
+		require(tvm.pubkey() != 0, 101);
+		// Check that message has signature (msg.pubkey() is not zero) and message is signed with the owner's private key
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+	}
+
+	modifier checkOwnerAndAccept {
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+		_;
+	}
+
+	function multiplication(uint value) public checkOwnerAndAccept {
+		result *= value;
+	}
+}
